69bf502ca352866fb2fc01fedbd44bfc2d956e70
¨Huu Khiem (Mark)¨ <tonghuukhiem@gmail.com>
2015-12-04 16:05:25 +0800

    update, now generate a random string based on unix timestamp

--- guid.go
+++ guid.go
@@ -1,37 +1,28 @@
 package nd
 
 import (
-  "fmt"
-  "errors"
-  "strings"
-  "crypto/rand"
-  "encoding/hex"
+	"strconv"
+	"time"
 )
 
 var Guidv4 func() []byte
 
 func init() {
-  ResetGuidv4()
+	ResetGuidv4()
 }
 
 func ResetGuidv4() {
-  Guidv4 = func() []byte {
-    t := make([]byte, 16)
-    rand.Read(t)
-    return t
-  }
+	Guidv4 = func() []byte {
+		return []byte(strconv.FormatInt(time.Now().UnixNano(), 10))
+	}
 }
 
 func Guidv4String() string {
-  t := Guidv4()
-  return fmt.Sprintf("%x-%x-%x-%x-%x", t[0:4], t[4:6], t[6:8], t[8:10], t[10:])
+	t := Guidv4()
+	return string(t)
 }
 
 func ForceGuid(guid string) error {
-  if len(guid) < 28 { return errors.New("Length of the GUID should be at least 28 hexadecimal characters") }
-  guid = strings.Replace(guid, "-", "", -1)
-  g, err := hex.DecodeString(guid)
-  if err != nil { return err }
-  Guidv4 = func() []byte { return g }
-  return nil
+	Guidv4 = func() []byte { return []byte(guid) }
+	return nil
 }
--- guid_test.go
+++ guid_test.go
@@ -1,47 +1,52 @@
 package nd
 
 import (
-  "bytes"
-  "regexp"
-  "testing"
+	// "bytes"
+	"regexp"
+	"testing"
 )
 
 func TestGuidv4ReturnsRandomGuids(t *testing.T) {
-  assertGuidv4IsRandom(t)
+	assertGuidv4IsRandom(t)
 }
 
 func TestGuidv4StringLooksOk(t *testing.T) {
-  guid := Guidv4String()
-  matched, _ := regexp.Match("[\\da-f]{8}\\-[\\da-f]{4}\\-[\\da-f]{4}\\-[\\da-f]{4}\\-[\\da-f]{8}", []byte(guid))
-  if matched == false { t.Error("Invalid format for guid string") }
+	guid := Guidv4String()
+	matched, _ := regexp.Match("^[0-9]{19}$", []byte(guid))
+	// matched, _ := regexp.Match("[0-9]{19}[\\da-f]{8}\\-[\\da-f]{4}\\-[\\da-f]{4}\\-[\\da-f]{4}\\-[\\da-f]{8}", []byte(guid))
+	if matched == false {
+		t.Error("Invalid format for guid string")
+	}
 }
 
 func TestCanForceAGuidv4(t *testing.T) {
-  expected := "aaaaaaaa-bbbb-cccc-dddd-bbbbbbbb"
-  expectedBytes := []byte{170,170,170,170,187,187,204,204,221,221,187,187,187,187}
-  ForceGuid(expected)
+	expected := "aaaaaaaa-bbbb-cccc-dddd-bbbbbbbb"
+	ForceGuid(expected)
 
-  guidBytes := Guidv4()
-  if bytes.Compare(guidBytes, expectedBytes) != 0 {
-    t.Errorf("Guid should be %q, got %q", expectedBytes, guidBytes)
-  }
+	guidBytes := Guidv4()
+	if string(guidBytes) != expected {
+		// if bytes.Compare(guidBytes, expectedBytes) != 0 {
+		t.Errorf("Guid should be %q, got %q", []byte(expected), guidBytes)
+	}
 
-  guidString := Guidv4String()
-  if guidString != "aaaaaaaa-bbbb-cccc-dddd-bbbbbbbb" {
-    t.Errorf("Guid string should be %q, got %q", expected, guidString)
-  }
+	guidString := Guidv4String()
+	if guidString != "aaaaaaaa-bbbb-cccc-dddd-bbbbbbbb" {
+		t.Errorf("Guid string should be %q, got %q", expected, guidString)
+	}
 }
 
 func TestCanResetGuidv4(t *testing.T) {
-  ForceGuid("aaaaaaaa-bbbb-cccc-dddd-bbbbbbbb")
-  ResetGuidv4()
-  assertGuidv4IsRandom(t)
+	ForceGuid("aaaaaaaa-bbbb-cccc-dddd-bbbbbbbb")
+	ResetGuidv4()
+	assertGuidv4IsRandom(t)
 }
 
 func assertGuidv4IsRandom(t *testing.T) {
-  seen := make(map[string]bool, 500)
-  for i := 0; i < 500; i++ {
-    seen[string(Guidv4())] = true
-  }
-  if len(seen) != 500 { t.Error("Should have seen 500 unique guids") }
+	seen := make(map[string]bool, 500)
+	for i := 0; i < 500; i++ {
+		seen[string(Guidv4())] = true
+	}
+	if len(seen) != 500 {
+		t.Error("Should have seen 500 unique guids")
+	}
 }
